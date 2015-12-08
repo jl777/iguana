2f46eafd8bc7008a399314fe52739c0559a5a17b
Vlad <black-owling@outlook.com>
2015-12-08 18:45:16 +0200

    added history of submits

--- js/api.js
+++ js/api.js
@@ -19,10 +19,6 @@ var SPNAPI = (function(SPNAPI, $, undefined) {
         });
         $(".page").hide();
         $("#welcome_page").show();
-    $(".submit_api_request").on("click", function () {
-    SPNAPI.submitRequest();
-    });
-
         $(".submit_api_request").on("click", function () {
 
             SPNAPI.submitRequest();
@@ -35,6 +31,8 @@ var SPNAPI = (function(SPNAPI, $, undefined) {
 
         });
 
+        
+
     });
 
     SPNAPI.submitRequest = function(e) {
@@ -42,13 +40,19 @@ var SPNAPI = (function(SPNAPI, $, undefined) {
 //code added in order to be able to receive input from <input> or <textarea> fields
 //original code: var request = $(".json_submit_url").html();
 //CODE CHANGED START
-if ($(".json_submit_url").html()) {
-        var request = $(".json_submit_url").html();
-}
-else if ($("#json_submit_url").val()) {
-	var request = $("#json_submit_url").val();
-}
+        // val() will not work with multiple textareas with same class
+        // when there will be one tab for talking with api - this if-else can be deleted
+        if ($(".json_submit_url").val()) {
+                var request = $(".json_submit_url").val();
+        }
+        else if ($("#json_src").val()) {
+        	var request = $("#json_src").val();
+        }
+       
 //CODE CHANGED STOP
+        // add new item in history table
+        historyTable.addItem(request);
+
         postCall('iguana', request, function(jsonstr)
         {
             $(".debuglogdebuglog").append(jsonstr);
@@ -58,5 +62,6 @@ else if ($("#json_submit_url").val()) {
 
         });
     };
+
     return SPNAPI;
 }(SPNAPI || {}, jQuery));
