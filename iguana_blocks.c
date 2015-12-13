b7cbc11109e0b27741c601b457436c904bc37842
jl777 <jameslee777@yahoo.com>
2015-12-13 20:03:43 -0300

    test

--- iguana_blocks.c
+++ iguana_blocks.c
@@ -1257,7 +1257,7 @@ int32_t iguana_issueloop(struct iguana_info *coin)
             nextbp = (i < coin->bundlescount-1) ? coin->bundles[i+1] : 0;
             if ( bp->emitfinish == 0 )
             {
-                iguana_bundlecheck(coin,bp,i == coin->closestbundle || i == lastbundle);
+                iguana_bundlecheck(coin,bp,numactive == 0 || i == coin->closestbundle || i == lastbundle);
                 if ( bp->numrecv > closest && bp->numrecv < coin->chain->bundlesize )
                 {
                     closest = bp->numrecv;
