3cb924e1b7ca7d1e7df96108e77d1a804676edef
jl777 <jameslee777@yahoo.com>
2015-12-22 07:34:26 -0300

    test

--- iguana_peers.c
+++ iguana_peers.c
@@ -939,8 +939,8 @@ void iguana_dedicatedloop(struct iguana_info *coin,struct iguana_peer *addr)
                 if ( poll(&fds,1,timeout) > 0 )
                     flag += iguana_pollQs(coin,addr);
             }
-            if ( flag == 0 )//&& iguana_processjsonQ(coin) == 0 )
-                usleep(100);//+ 100000*(coin->blocks.hwmheight > (long)coin->longestchain-coin->minconfirms*2));
+            if ( 0 && flag == 0 )//&& iguana_processjsonQ(coin) == 0 )
+                usleep(1000);//+ 100000*(coin->blocks.hwmheight > (long)coin->longestchain-coin->minconfirms*2));
         }
     }
     if ( addr->fp != 0 )
