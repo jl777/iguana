9eeaf484a71b5e22351421dcc1ae510d87803fc1
jl777 <jameslee777@yahoo.com>
2015-12-22 18:02:50 -0300

    test

--- iguana_recv.c
+++ iguana_recv.c
@@ -102,10 +102,11 @@ void iguana_gotblockM(struct iguana_info *coin,struct iguana_peer *addr,struct i
                     struct iguana_txblock *checktxdata; struct iguana_memspace checkmem; int32_t checkbundlei;
                     memset(&checkmem,0,sizeof(checkmem));
                     iguana_meminit(&checkmem,"checkmem",0,txdata->datalen + 4096,0);
-                    if ( 0 && (checktxdata= iguana_peertxdata(coin,&checkbundlei,fname,&checkmem,addr->ipbits,txdata->block.hash2)) != 0 )
+                    if ( (checktxdata= iguana_peertxdata(coin,&checkbundlei,fname,&checkmem,addr->ipbits,txdata->block.hash2)) != 0 )
                     {
                         printf("check datalen.%d bundlei.%d T.%d U.%d S.%d P.%d X.%d\n",checktxdata->datalen,checkbundlei,checktxdata->numtxids,checktxdata->numunspents,checktxdata->numspends,checktxdata->numpkinds,checktxdata->numexternaltxids);
                     }
+                    iguana_mempurge(&checkmem);
                 }
             }
          } else txdata = origtxdata;
