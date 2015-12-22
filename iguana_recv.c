f7af74c08e74b6c36fadac66364c8206842146c0
jl777 <jameslee777@yahoo.com>
2015-12-22 05:34:29 -0300

    test

--- iguana_recv.c
+++ iguana_recv.c
@@ -335,7 +335,7 @@ int32_t iguana_processbundlesQ(struct iguana_info *coin,int32_t *newhwmp) // sin
     *newhwmp = 0;
     while ( flag < 1 && (req= queue_dequeue(&coin->bundlesQ,0)) != 0 )
     {
-        //printf("%s bundlesQ.%p type.%c n.%d\n",req->addr != 0 ? req->addr->ipaddr : "0",req,req->type,req->n);
+        printf("%s bundlesQ.%p type.%c n.%d\n",req->addr != 0 ? req->addr->ipaddr : "0",req,req->type,req->n);
         if ( req->type == 'B' ) // one block with all txdata
             req = iguana_recvblock(coin,req->addr,req,&req->block,req->numtx,req->datalen,newhwmp);
         else if ( req->type == 'H' ) // blockhdrs (doesnt have txn_count!)
@@ -360,6 +360,7 @@ int32_t iguana_processbundlesQ(struct iguana_info *coin,int32_t *newhwmp) // sin
         }
         else printf("iguana_updatebundles unknown type.%c\n",req->type);
         flag++;
+        printf("done %s bundlesQ.%p type.%c n.%d\n",req->addr != 0 ? req->addr->ipaddr : "0",req,req->type,req->n);
         if ( req != 0 )
             myfree(req,req->allocsize), req = 0;
     }
