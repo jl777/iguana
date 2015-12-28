9d1e957de3040becf6b9d5d5aa1594e91ff007fe
jl777 <jameslee777@yahoo.com>
2015-12-28 07:37:51 -0300

    test

--- iguana_recv.c
+++ iguana_recv.c
@@ -713,7 +713,7 @@ int32_t iguana_processrecv(struct iguana_info *coin) // single threaded
     if ( (next= iguana_blockfind(coin,iguana_blockhash(coin,coin->blocks.hwmchain.height+1))) != 0 )
     {
         _iguana_chainlink(coin,next);
-        if ( (coin->blocks.hwmchain.height+1 < coin->longestchain && coin->backstop != coin->blocks.hwmchain.height+1) || milliseconds() > coin->backstop+1000 )
+        if ( (coin->blocks.hwmchain.height+1 < coin->longestchain && coin->backstop != coin->blocks.hwmchain.height+1) || milliseconds() > coin->backstopmillis+100 )
         {
             coin->backstop = coin->blocks.hwmchain.height+1;
             coin->backstophash2 = next->hash2;
