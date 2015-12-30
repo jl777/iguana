91ab5f61d98c55bd8ea07f9d70b4a7af634acdb4
jl777 <jameslee777@yahoo.com>
2015-12-30 08:20:50 -0300

    test

--- iguana_peers.c
+++ iguana_peers.c
@@ -926,7 +926,7 @@ int64_t iguana_peerallocated(struct iguana_info *coin,struct iguana_peer *addr)
 
 void iguana_dedicatedloop(struct iguana_info *coin,struct iguana_peer *addr)
 {
-    struct pollfd fds; struct iguana_cacheptr *ptr; uint8_t *buf,serialized[64];
+    struct pollfd fds; uint8_t *buf,serialized[64];
     int32_t bufsize,flag,timeout = strcmp(coin->symbol,"BTC") == 0 ? 3 : 50;//coin->MAXPEERS/64+1;
 #ifdef IGUANA_PEERALLOC
     int32_t i;  int64_t remaining; struct iguana_memspace *mem[sizeof(addr->SEROUT)/sizeof(*addr->SEROUT)];
@@ -998,7 +998,7 @@ void iguana_dedicatedloop(struct iguana_info *coin,struct iguana_peer *addr)
             if ( flag == 0 )//&& iguana_processjsonQ(coin) == 0 )
             {
                 if ( addr->rank != 1 )
-                    usleep(5000 + (rand() % 10000));
+                    usleep(1000 + (rand() % 1000));
                 else usleep(100);
             }
         }
